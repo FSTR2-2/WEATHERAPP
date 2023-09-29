48531490b88670af194ea9603093b915be51a5ec
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-09-29 16:35:51 +0300

    add air quality data into getData function

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,14 +1,24 @@
 async function getData(city='istanbul') {
   const locationData = await getLocationData(city);
   const { latitude, longitude, area, country } = locationData;
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,relativehumidity_2m&daily=weathercode&current_weather=true&timezone=auto&forecast_days=2`;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode,uv_index&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto&forecast_days=14`;
+  const aqUrl = `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${latitude}&longitude=${longitude}&hourly=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone&timezone=auto`
 
   const data = await fetch(url).then((response) => response.json());
+  const aqData = await fetch(aqUrl).then((response) => response.json()); //! Data for AirQuality
+
+  const mergedData = {
+    ...data,
+    hourly: {
+      ...data.hourly,
+      ...aqData.hourly
+    }
+  }
 
   return {
     area: area,
     country: country,
-    ...data,
+    ...mergedData,
   };
 }
 
