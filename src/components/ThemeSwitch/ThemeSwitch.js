e3f1371081e0ac28252fe407a7393852aabacf10
hsrvms <59848940+hsrvms@users.noreply.github.com>
2023-10-10 15:26:36 +0300

    add/dark-mode btn skeleton

--- src/components/ThemeSwitch/ThemeSwitch.js
+++ src/components/ThemeSwitch/ThemeSwitch.js
@@ -1,7 +1,25 @@
+import './ThemeSwitch.css'
+
 const ThemeSwitch = (element) => {
  element.innerHTML = `
-  <div>ThemeSwitch</div>
+  <div class="darkmode-btn">
+    <div class="cycle"></div>
+  </div>
  `;
+
+  const darkmodeBtn = document.querySelector('.darkmode-btn');
+  const darkmodeCycle = darkmodeBtn.querySelector('.cycle')
+
+  darkmodeBtn.addEventListener('click', handleDarkMode)
+
+ function handleDarkMode() {
+  console.log(darkmodeBtn, darkmodeCycle)
+  darkmodeBtn.classList.toggle('active')
+
+ }
+
+ 
 }
+
 
 export default ThemeSwitch;
